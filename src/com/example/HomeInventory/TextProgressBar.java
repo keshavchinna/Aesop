220139f3418812a9927ff26dc67657396650aeae
chennakeshav <keshav@eggheadcreative.com>
2014-09-22 16:30:04 +0530

    changed text color to black to white in TextProgressBar

--- src/com/example/HomeInventory/TextProgressBar.java
+++ src/com/example/HomeInventory/TextProgressBar.java
@@ -9,48 +9,48 @@ import android.util.AttributeSet;
 import android.widget.ProgressBar;
 
 public class TextProgressBar extends ProgressBar {
-    private String text;
-    private Paint textPaint;
+  private String text;
+  private Paint textPaint;
 
-    public TextProgressBar(Context context) {
-        super(context);
-        text = "HP";
-        textPaint = new Paint();
-        textPaint.setColor(Color.BLACK);
-    }
+  public TextProgressBar(Context context) {
+    super(context);
+    text = "0%";
+    textPaint = new Paint();
+    textPaint.setColor(Color.WHITE);
+  }
 
-    public TextProgressBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        text = "HP";
-        textPaint = new Paint();
-        textPaint.setColor(Color.BLACK);
-    }
+  public TextProgressBar(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    text = "0%";
+    textPaint = new Paint();
+    textPaint.setColor(Color.WHITE);
+  }
 
-    public TextProgressBar(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        text = "HP";
-        textPaint = new Paint();
-        textPaint.setColor(Color.BLACK);
-    }
+  public TextProgressBar(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    text = "0%";
+    textPaint = new Paint();
+    textPaint.setColor(Color.WHITE);
+  }
 
-    @Override
-    protected synchronized void onDraw(Canvas canvas) {
-        // First draw the regular progress bar, then custom draw our text
-        super.onDraw(canvas);
-        Rect bounds = new Rect();
-        textPaint.getTextBounds(text, 0, text.length(), bounds);
-        int x = getWidth() / 2 - bounds.centerX();
-        int y = getHeight() / 2 - bounds.centerY();
-        canvas.drawText(text, x, y, textPaint);
-    }
+  @Override
+  protected synchronized void onDraw(Canvas canvas) {
+    // First draw the regular progress bar, then custom draw our text
+    super.onDraw(canvas);
+    Rect bounds = new Rect();
+    textPaint.getTextBounds(text, 0, text.length(), bounds);
+    int x = getWidth() / 2 - bounds.centerX();
+    int y = getHeight() / 2 - bounds.centerY();
+    canvas.drawText(text, x, y, textPaint);
+  }
 
-    public synchronized void setText(String text) {
-        this.text = text;
-        drawableStateChanged();
-    }
+  public synchronized void setText(String text) {
+    this.text = text;
+    drawableStateChanged();
+  }
 
-    public void setTextColor(int color) {
-        textPaint.setColor(color);
-        drawableStateChanged();
-    }
+  public void setTextColor(int color) {
+    textPaint.setColor(color);
+    drawableStateChanged();
+  }
 }
