49d8cde10aa934d917f6aa816acf685a6b18c875
keshavchinna <y.keshav@rocketmail.com>
2014-10-12 23:15:26 +0530

    implemented webview graph with dummy content

--- src/com/bizconit/aesop/activity/GraphActivity.java
+++ src/com/bizconit/aesop/activity/GraphActivity.java
@@ -1,20 +1,19 @@
 package com.bizconit.aesop.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.app.Application;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.*;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.bizconit.aesop.R;
-import com.google.gson.Gson;
-import com.jjoe64.graphview.*;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.bizconit.aesop.helper.ArrayApplication;
+import com.jjoe64.graphview.GraphView;
+import org.json.JSONException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,24 +23,59 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class GraphActivity extends Activity {
-  private String[] dates;
-  private int[] values;
-  private String productName;
-  private GraphView.GraphViewData[] graphData;
+    private String[] dates;
+    private int[] values;
+    private String productName;
+    private GraphView.GraphViewData[] graphData;
 
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.graph_layout);
-    dates = getIntent().getStringArrayExtra("dates");
-    values = getIntent().getIntArrayExtra("values");
-    productName = getIntent().getStringExtra("productName");
-    getActionBar().setTitle(productName);
-    LinearLayout rootView = (LinearLayout) findViewById(R.id.root);
-    graphData = new GraphView.GraphViewData[values.length];
-    for (int i = 0; i < values.length; i++) {
-      graphData[i] = new GraphView.GraphViewData(i + 1, values[i]);
+    @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.graph_layout);
+        dates = getIntent().getStringArrayExtra("dates");
+        values = getIntent().getIntArrayExtra("values");
+        productName = getIntent().getStringExtra("productName");
+        getActionBar().setTitle(productName);
+
+        final WebView webView = (WebView) this.findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        Application application = getApplication();
+        webView.addJavascriptInterface(application, "android");
+        ((ArrayApplication) application).setActivity(this);
+        String s = ((ArrayApplication) application).a1dToJson(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new int[]{45, 87, 32, 85, 75, 41, 36, 87, 25, 65});
+        String s1 = ((ArrayApplication) application).a1dToJson(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new int[]{40, 25, 86, 32, 45, 20, 92, 56, 95, 50});
+        Log.d("test2", "String S: " + s);
+        try {
+            ((ArrayApplication) application).setData(s);
+            ((ArrayApplication) application).setData1(s1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        webView.requestFocusFromTouch();
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                webView.loadUrl("javascript:showData()");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:showData()");
+
+            }
+        });
+        webView.loadUrl("file:///android_asset/my.html");
     }
-    GraphViewSeries exampleSeries1 = new GraphViewSeries("Consumption", new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(90, 250, 00), 3), new GraphView.GraphViewData[]{
+    /*
+LinearLayout rootView = (LinearLayout) findViewById(R.id.root);
+graphData = new GraphView.GraphViewData[values.length];
+        for (int i = 0; i < values.length; i++) {
+        graphData[i] = new GraphView.GraphViewData(i + 1, values[i]);
+        }
+        GraphViewSeries exampleSeries1 = new GraphViewSeries("Consumption", new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(90, 250, 00), 3), new GraphView.GraphViewData[]{
         new GraphView.GraphViewData(1, 52)
         , new GraphView.GraphViewData(2, 36)
         , new GraphView.GraphViewData(3, 45)
@@ -51,9 +85,9 @@ public class GraphActivity extends Activity {
         , new GraphView.GraphViewData(7, 42)
         , new GraphView.GraphViewData(8, 88)
         , new GraphView.GraphViewData(9, 89)
-    });
-    GraphViewSeries.GraphViewSeriesStyle seriesStyle = new GraphViewSeries.GraphViewSeriesStyle();
-    GraphViewSeries exampleSeries2 = new GraphViewSeries("Consumption", new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(153, 51, 255), 3), new GraphView.GraphViewData[]{
+        });
+        GraphViewSeries.GraphViewSeriesStyle seriesStyle = new GraphViewSeries.GraphViewSeriesStyle();
+        GraphViewSeries exampleSeries2 = new GraphViewSeries("Consumption", new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(153, 51, 255), 3), new GraphView.GraphViewData[]{
         new GraphView.GraphViewData(1, 26)
         , new GraphView.GraphViewData(2, 35)
         , new GraphView.GraphViewData(3, 45)
@@ -63,30 +97,31 @@ public class GraphActivity extends Activity {
         , new GraphView.GraphViewData(7, 85)
         , new GraphView.GraphViewData(8, 92)
         , new GraphView.GraphViewData(9, 46)
-    });
-    GraphViewSeries exampleSeries = new GraphViewSeries(productName, new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), graphData);
-    LineGraphView graphView = new LineGraphView(this, productName + " Inventory");
+        });
+        GraphViewSeries exampleSeries = new GraphViewSeries(productName, new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3), graphData);
+        LineGraphView graphView = new LineGraphView(this, productName + " Inventory");
 
-    graphView.addSeries(exampleSeries);
-    graphView.addSeries(exampleSeries1);
-    graphView.addSeries(exampleSeries2);
+        graphView.addSeries(exampleSeries);
+        graphView.addSeries(exampleSeries1);
+        graphView.addSeries(exampleSeries2);
 
-    graphView.setManualYAxisBounds(100, 0);
-    graphView.setHorizontalLabels(dates);
-    graphView.setDrawDataPoints(true);
-    graphView.setScalable(true);
-    graphView.setShowLegend(true);
-    graphView.setLegendAlign(GraphView.LegendAlign.TOP);
-    graphView.setScrollable(true);
-    graphView.getGraphViewStyle().setVerticalLabelsAlign(Paint.Align.LEFT);
-    graphView.setDataPointsRadius(5.0f);
-    graphView.setDrawBackground(true);
-    graphView.getGraphViewStyle().setGridColor(Color.RED);
+        graphView.setManualYAxisBounds(100, 0);
+        graphView.setHorizontalLabels(dates);
+        graphView.setDrawDataPoints(true);
+        graphView.setScalable(true);
+        graphView.setShowLegend(true);
+        graphView.setLegendAlign(GraphView.LegendAlign.TOP);
+        graphView.setScrollable(true);
+        graphView.getGraphViewStyle().setVerticalLabelsAlign(Paint.Align.LEFT);
+        graphView.setDataPointsRadius(5.0f);
+        graphView.setDrawBackground(true);
+        graphView.getGraphViewStyle().setGridColor(Color.RED);
 //    graphView.setBackgroundColor(Color.GREEN);
-    graphView.getGraphViewStyle().setTextSize(14);
-    graphView.getGraphViewStyle().getGridStyle().drawHorizontal();
-    graphView.getGraphViewStyle().setNumHorizontalLabels(dates.length);
-    graphView.getGraphViewStyle().setNumVerticalLabels(11);
-    rootView.addView(graphView);
-  }
+        graphView.getGraphViewStyle().setTextSize(14);
+        graphView.getGraphViewStyle().getGridStyle().drawHorizontal();
+        graphView.getGraphViewStyle().setNumHorizontalLabels(dates.length);
+        graphView.getGraphViewStyle().setNumVerticalLabels(11);
+        rootView.addView(graphView);*/
 }
+
+
---
+++ src/com/bizconit/aesop/helper/ArrayApplication.java
@@ -0,0 +1,107 @@
+package com.bizconit.aesop.helper;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class ArrayApplication extends Application {
+
+  static final String TAG = "JavascriptDataDemo";
+
+  int[] data = new int[10];
+  int[] data1 = new int[10];
+  int[] data3 = new int[10];
+  int[] data2 = new int[]{40, 25, 86, 32, 45, 20, 92, 56, 95, 50};
+
+  /**
+   * This passes our data out to the JS
+   */
+  @JavascriptInterface
+  public String getData1() {
+    Log.d(TAG, "getData() called");
+    return a1dToJson(data2, data3).toString();
+  }
+
+  @JavascriptInterface
+  public String getData() {
+    Log.d(TAG, "getData() called");
+    return a1dToJson(data, data1).toString();
+  }
+
+  /**
+   * Allow the JavaScript to pass some data in to us.
+   */
+  @JavascriptInterface
+  public void setData(String newData) throws JSONException {
+    Log.d("test2", newData);
+    Log.d(TAG, "MainActivity.setData()");
+    JSONArray streamer = new JSONArray(newData);
+    data = new int[streamer.length()];
+    data1 = new int[streamer.length()];
+    for (int i = 0; i < streamer.length(); i++) {
+      String n = streamer.getString(i);
+      String[] sarray = n.replace("[", "").replace("]", "").split(",");
+      data[i] = Integer.parseInt(sarray[0]);
+      data1[i] = Integer.parseInt(sarray[1]);
+    }
+  } @JavascriptInterface
+  public void setData1(String newData) throws JSONException {
+    Log.d("test2", newData);
+    Log.d(TAG, "MainActivity.setData()");
+    JSONArray streamer = new JSONArray(newData);
+    data2 = new int[streamer.length()];
+    data3 = new int[streamer.length()];
+    for (int i = 0; i < streamer.length(); i++) {
+      String n = streamer.getString(i);
+      String[] sarray = n.replace("[", "").replace("]", "").split(",");
+      data2[i] = Integer.parseInt(sarray[0]);
+      data3[i] = Integer.parseInt(sarray[1]);
+    }
+  }
+
+  private Activity activity;
+
+  public Context getActivity() {
+    return activity;
+  }
+
+  public void setActivity(Activity app) {
+    this.activity = app;
+  }
+
+  @JavascriptInterface
+  public void finish() {
+    Log.d(TAG, "ArrayApplication.finish()");
+    activity.finish();
+  }
+
+  /**
+   * Sorry for not using the standard org.json.JSONArray but even in Android 4.2 it lacks
+   * the JSONArray(Object[]) constructor, making it too painful to use.
+   *
+   * @param data
+   * @param doubles
+   */
+  public String a1dToJson(int[] data, int[] doubles) {
+    StringBuffer sb = new StringBuffer();
+    sb.append("[");
+    for (int i = 0; i < data.length; i++) {
+      sb.append("[");
+      int d = data[i];
+      int d1 = doubles[i];
+
+      sb.append(d);
+      sb.append(",");
+      sb.append(d1);
+      sb.append("]");
+      if ((i + 1) < data.length)
+        sb.append(",");
+    }
+    sb.append("]");
+    return sb.toString();
+  }
+}
